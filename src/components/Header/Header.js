fbfee1147eaba2406b8875cc0226cb46d008d043
emonShimoul <emonshimoul@gmail.com>
2022-04-19 10:03:55 +0600

    start a project named ema-john-simpe-react

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>This is Header</h2>
+            <img src={logo} />
+        </div>
+    );
+};
+
+export default Header;
