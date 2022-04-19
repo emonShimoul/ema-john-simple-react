2ae1a916b2ecd9f7313963287f204bbee76ba1a4
emonShimoul <emonshimoul@gmail.com>
2022-04-19 10:39:00 +0600

    edited shop component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Shop.css';
 
 const Shop = () => {
     return (
         <div>
-            <h2>This is Shop</h2>
+            <div className="shop-container">
+                <div className="product-container">
+                    <h3>Products</h3>
+                </div>
+                <div className="cart-container">
+                    <h3>Order Summary</h3>
+                    <h5>Items Ordered:</h5>
+                </div>
+            </div>
         </div>
     );
 };
