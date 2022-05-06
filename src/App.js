fc4f176d333eee5151245ccd41a772fda0aa6185
emonShimoul <emonshimoul@gmail.com>
2022-05-06 12:07:26 +0600

    install react router and implement simple router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Shop />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/orders" element={<OrderReview></OrderReview>}></Route>
+          <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
