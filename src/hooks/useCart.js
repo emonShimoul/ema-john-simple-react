7457d2cb57fbee87a0dd1ce06a792ae1c4a389a4
emonShimoul <emonshimoul@gmail.com>
2022-05-06 12:45:59 +0600

    create useCart and use it

---
+++ src/hooks/useCart.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { getStoredCart } from "../utilities/fakedb";
+
+const useCart = products => {
+    const [cart, setCart] = useState([]);
+    // console.log(cart);
+    useEffect(() => {
+        if(products.length){
+            const savedCart = getStoredCart();
+            // console.log(savedCart);
+            const storedCart = [];
+            for(const key in savedCart){
+                const addedProduct = products.find(product => product.key == key);
+                // console.log(addedProduct);
+                if(addedProduct){
+                    // set quantity
+                    const quantity = savedCart[key];
+                    addedProduct.quantity = quantity;
+                    storedCart.push(addedProduct);
+                    // console.log(storedCart);
+                }
+            }
+            setCart(storedCart);
+        }
+    }, [products]);
+    return [cart];
+}
+
+export default useCart;
