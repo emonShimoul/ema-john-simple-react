aeb6afd7a382018fd1470bba1e3408418a31d278
emonShimoul <emonshimoul@gmail.com>
2022-05-08 12:25:21 +0600

    display review items in Order Review page

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import useProducts from '../../hooks/useProducts';
 import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart'
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
     const [products, setProducts] = useProducts();
@@ -9,7 +10,9 @@ const OrderReview = () => {
     return (
         <div className='shop-container'>
             <div className='product-container'>
-                
+                {
+                    cart.map(product => <ReviewItem product={product}></ReviewItem>)
+                }
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
