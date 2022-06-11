d075225eff45394e3fe895afde29e63e721c4154
emonShimoul <emonshimoul@gmail.com>
2022-06-11 11:35:54 +0600

    login, logout functionality completed in the UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
+    const {user, logOut} = useFirebase();
     return (
         <div className='header'>
-            <img className='logo' src={logo} />
+            <img className='logo' src={logo} alt="" />
             <nav>
                 <NavLink to="/shop">Shop</NavLink>
                 <NavLink to="/orders">Order Review</NavLink>
                 <NavLink to="/inventory">Manage Inventory</NavLink>
-                <NavLink to="/login">Login</NavLink>
+                {
+                    user.email ?
+                    <button onClick={logOut}>Log Out</button>
+                    :
+                    <NavLink to="/login">Login</NavLink>
+                }
             </nav>
         </div>
     );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import initializeAuthentication from "../Firebase/firebase.init";
+
+initializeAuthentication();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
@@ -21,7 +24,7 @@ const useFirebase = () => {
         })
     }
 
-    // firebase observer
+    // firebase observer (observe whether user auth state changed or not )
     useEffect(() => {
         onAuthStateChanged(auth, user => {
             if(user){
