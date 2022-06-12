61327ade450084392aec8b89b62f70b52810804b
emonShimoul <emonshimoul@gmail.com>
2022-06-12 12:28:10 +0600

    added fields in the react hook form

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
 import './Shipping.css';
 
 const Shipping = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const {user} = useAuth();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
         <div>
             <form className='shipping-form' onSubmit={handleSubmit(onSubmit)}>
 
-                <input defaultValue="test" {...register("example")} />
-                
-                <input {...register("exampleRequired", { required: true })} />
+                <input defaultValue={user.displayName} {...register("name")} />
+                <input defaultValue={user.email} {...register("email", { required: true })} />
 
-                {errors.exampleRequired && <span>This field is required</span>}
+                {errors.email && <span className='error'>This field is required</span>}
+                <input placeholder='Address' defaultValue="" {...register("address")} />
+                <input placeholder='City' defaultValue="" {...register("city")} />
+                <input placeholder='Phone Number' defaultValue="" {...register("phone")} />
                 
                 <input type="submit" />
             </form>
